c96b5dfa614b3cea1c8271158e0447323ff466d4
eeeseohyun <eeeseohyun@gmail.com>
2023-03-06 14:27:51 +0900

    1주차 과제

---
+++ Java_1_2.java
@@ -0,0 +1,13 @@
+
+public class Java_1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		for(int i=1; i<11; i++) {
+			sum = sum+i;
+		}
+		System.out.print(sum);
+	}
+
+}
